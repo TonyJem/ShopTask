7723f424b2086c66840d5c1c48c65e67114e7ab1
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-26 14:57:32 +0300

    Tested structure work
    new Shop initialized - Ok
    Shop's working hours and schedule works - Ok

--- ShopTask/ViewController.swift
+++ ShopTask/ViewController.swift
@@ -20,26 +20,47 @@ class ViewController: NSViewController {
     var saturdayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
     var sundayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
     
-    var schedule: Shop.WorkSchedule!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(mondayWorkHours)
         
-        schedule = Shop.WorkSchedule(monday: mondayWorkHours,
+        let schedule = Shop.WorkSchedule(monday: mondayWorkHours,
                                      tuesday: tuesdayWorkHours,
                                      wednesday: wednesdayWorkHours,
                                      thursday: thursdayWorkHours,
-                                     friday: fridayWorkHours,
+                                     friday: nil,
                                      saturday: saturdayWorkHours,
                                      sunday: sundayWorkHours)
         if let mondayHours = schedule.monday{
             print(mondayHours)
         } else {
-            print("closed")
+            print("Closed")
         }
+        
+        if let fridayHours = schedule.friday{
+            print(fridayHours)
+        } else {
+            print("Closed")
+        }
+        
+        testShop = Shop(name: "TestName", address: "TestAddress", workSchedule: schedule)
+        
+        print(testShop.name)
+        print(testShop.address)
+       
+        if let mondayHours = testShop.workSchedule.monday{
+            print("Shop say: \(mondayHours)")
+        } else {
+            print("Shop say: Closed")
+        }
+        
+        if let fridayHours = testShop.workSchedule.friday{
+            print("Shop say: \(fridayHours)")
+        } else {
+            print("Shop say: Closed")
+        }
+        
         
         //         Do any additional setup after loading the view.
     }
