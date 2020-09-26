2e7b877a5e8251addb81eaec57c6482819700107
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-26 11:06:57 +0300

    Initial Commit

---
+++ ShopTask/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ShopTask
+//
+//  Created by Anton on 2020-09-26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
