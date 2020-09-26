616f37fd1995bd607611b35cb9d2b69a39fced02
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-26 14:45:20 +0300

    Update Structure "Shop" to make it same as it is provided in a Task

--- ShopTask/Shop.swift
+++ ShopTask/Shop.swift
@@ -11,21 +11,19 @@ struct Shop{
     let name: String
     let address: String
     let workSchedule: WorkSchedule
+    
+    struct WorkSchedule {
+        var monday: WorkHours?
+        var tuesday: WorkHours?
+        var wednesday: WorkHours?
+        var thursday: WorkHours?
+        var friday: WorkHours?
+        var saturday: WorkHours?
+        var sunday: WorkHours?
+    }
+
+    struct WorkHours {
+        let from: String
+        let to: String
+    }
 }
-
-struct WorkSchedule {
-    var monday: WorkHours?
-    var tuesday: WorkHours?
-    var wednesday: WorkHours?
-    var thursday: WorkHours?
-    var friday: WorkHours?
-    var saturday: WorkHours?
-    var sunday: WorkHours?
-}
-
-struct WorkHours {
-    let from: String
-    let to: String
-}
-
-
--- ShopTask/ViewController.swift
+++ ShopTask/ViewController.swift
@@ -9,44 +9,47 @@ import Foundation
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     var testShop: Shop!
     
-    var mondayWorkHours = WorkHours(from: "09:00", to: "21:00")
-    var tuesdayWorkHours = WorkHours(from: "09:00", to: "21:00")
-    var wednesdayWorkHours = WorkHours(from: "09:00", to: "21:00")
-    var thursdayWorkHours = WorkHours(from: "10:00", to: "21:00")
-    var fridayWorkHours = WorkHours(from: "", to: "")
-    var saturdayWorkHours = WorkHours(from: "09:00", to: "21:00")
-    var sundayWorkHours = WorkHours(from: "09:00", to: "21:00")
+    var mondayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
+    var tuesdayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
+    var wednesdayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
+    var thursdayWorkHours = Shop.WorkHours(from: "10:00", to: "21:00")
+    var fridayWorkHours = Shop.WorkHours(from: "", to: "")
+    var saturdayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
+    var sundayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
     
-    var schedule: WorkSchedule!
-
-    
+    var schedule: Shop.WorkSchedule!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         print(mondayWorkHours)
         
-        
-        
-        schedule = WorkSchedule(monday: mondayWorkHours, tuesday: tuesdayWorkHours, wednesday: wednesdayWorkHours, thursday: thursdayWorkHours, friday: fridayWorkHours, saturday: saturdayWorkHours, sunday: sundayWorkHours)
+        schedule = Shop.WorkSchedule(monday: mondayWorkHours,
+                                     tuesday: tuesdayWorkHours,
+                                     wednesday: wednesdayWorkHours,
+                                     thursday: thursdayWorkHours,
+                                     friday: fridayWorkHours,
+                                     saturday: saturdayWorkHours,
+                                     sunday: sundayWorkHours)
         if let mondayHours = schedule.monday{
             print(mondayHours)
         } else {
             print("closed")
         }
         
-        // Do any additional setup after loading the view.
+        //         Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
