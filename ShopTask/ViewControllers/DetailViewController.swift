39bb1cfbd6e3a01a3c3021904470020018f4d2b6
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-26 22:35:52 +0300

    Expanding UI
    Add SplitViewController and it's views
    Created new files for these controllers

---
+++ ShopTask/ViewControllers/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  ShopTask
+//
+//  Created by Office-iMac on 2020-09-26.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ ShopTask/ViewControllers/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  ShopTask
+//
+//  Created by Office-iMac on 2020-09-26.
+//
+
+import Cocoa
+
+class TableViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
