6a5c50a1f96afc4a8e056a3662a2aa0313febc71
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-09-26 20:45:09 +0300

    Finished rought solution and tested via print

--- ShopTask/ViewController.swift
+++ ShopTask/ViewController.swift
@@ -26,10 +26,10 @@ class ViewController: NSViewController {
     var sundayWorkHours = Shop.WorkHours(from: "09:00", to: "21:00")
     
     var hoursAsStingInArray: [String] = []
-    let daysAsStingInArray: [String] = ["I","II","III","IV","V","VI","VII"]
-    
-    var daysToShow: [String] = []
-    var hoursToShow: [String] = []
+    var hoursAsStingInArrayUpdated: [String] = []
+    var registeredLeftItems: [Int] = []
+    var dayRanges: [String] = []
+    var dayRangesRomanian: [String] = []
     
     //    MARK: - Start Here
     override func viewDidLoad() {
@@ -48,10 +48,27 @@ class ViewController: NSViewController {
         
         populateWorkingHoursArray()
         
-        createResults()
-        print(hoursToShow)
-        print(daysToShow)
+        for (index, value) in hoursAsStingInArray.enumerated() {
+            print("Day \(index + 1): \(value)")
+        }
         
+        createResults()
+        
+        print("------Updated Hours array:------")
+        for (index, value) in hoursAsStingInArrayUpdated.enumerated() {
+            print("Day \(index + 1): \(value)")
+        }
+        
+        print("Days in list: \(registeredLeftItems)")
+        
+        
+        createRangeStrings()
+        print(dayRanges)
+        
+        formatStringFiguresToRomanianStyle()
+        print(dayRangesRomanian)
+        
+        print(hoursAsStingInArrayUpdated)
     }
     
     override var representedObject: Any? {
@@ -80,25 +97,68 @@ class ViewController: NSViewController {
     }
     
     func createResults(){
-        daysToShow.removeAll()
-        hoursToShow.removeAll()
-        
-        hoursToShow.append(hoursAsStingInArray[0])
-        daysToShow.append(daysAsStingInArray[0])
-        
-        for item in (1...6) {
-            if hoursAsStingInArray[item] != hoursAsStingInArray[item - 1] {
-                hoursToShow.append(hoursAsStingInArray[item])
-                daysToShow.append(daysAsStingInArray[item])
+        registeredLeftItems.append(1)
+        hoursAsStingInArrayUpdated.append(hoursAsStingInArray[0])
+        for item in 1...6{
+            if hoursAsStingInArray[item] == hoursAsStingInArray[item - 1]{
+//                hoursAsStingInArrayUpdated.append("")
             } else {
-                daysToShow.append("")
+                hoursAsStingInArrayUpdated.append(hoursAsStingInArray[item])
+                registeredLeftItems.append(item + 1)
             }
         }
-        
     }
+    
+    
+    func createRangeStrings(){
+        if registeredLeftItems.count > 1{
+            for item in 0..<registeredLeftItems.count - 1 {
+                if registeredLeftItems[item] == registeredLeftItems[item + 1] - 1{
+                    dayRanges.append("\(registeredLeftItems[item])-\(registeredLeftItems[item])")
+                } else {
+                    dayRanges.append("\(registeredLeftItems[item])-\(registeredLeftItems[item + 1] - 1)")
+                }
+            }
+        }
+        dayRanges.append("\(registeredLeftItems[registeredLeftItems.count - 1])-7")
+    }
+    
+    func formatStringFiguresToRomanianStyle(){
+        for range in dayRanges{
+            var rangeToRomanian = range
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "1", with: "I")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "2", with: "II")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "3", with: "III")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "4", with: "IV")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "5", with: "V")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "6", with: "VI")
+            rangeToRomanian = rangeToRomanian.replacingOccurrences(of: "7", with: "VII")
+            
+            switch rangeToRomanian {
+            case "I-I":
+                rangeToRomanian = "I"
+                case "II-II":
+                rangeToRomanian = "II"
+                case "III-III":
+                rangeToRomanian = "III"
+                case "IV-IV":
+                rangeToRomanian = "IV"
+                case "V-V":
+                rangeToRomanian = "V"
+                case "VI-VI":
+                rangeToRomanian = "VI"
+                case "VII-VII":
+                rangeToRomanian = "VII"
+            default: break
+            }
+            
+            dayRangesRomanian.append(rangeToRomanian)
+        }
+    }
+    
+    
     
     
     
     //    MARK: - Actions
 }
-
