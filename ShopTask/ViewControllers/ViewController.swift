a5156e48b61f4c504c31aa23b1ec2dbd18ece640
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-10-02 10:58:51 +0300

    Create Actions and Outlets for elements in "Shop details" window

--- ShopTask/ViewControllers/ViewController.swift
+++ ShopTask/ViewControllers/ViewController.swift
@@ -10,6 +10,44 @@ import Cocoa
 
 class ViewController: NSViewController {
     //    MARK: - Outlets
+    @IBOutlet var shopNameTextField: NSTextField!
+    @IBOutlet var shopAddressTextField: NSTextField!
+    
+    @IBOutlet var mondayBtn: NSButton!
+    @IBOutlet var tuesdayBtn: NSButton!
+    @IBOutlet var wednesdayBtn: NSButton!
+    @IBOutlet var thursdayBtn: NSButton!
+    @IBOutlet var fridayBtn: NSButton!
+    @IBOutlet var saturdayBtn: NSButton!
+    @IBOutlet var sundayBtn: NSButton!
+    
+    @IBOutlet var mondayOpenTimePicker: NSDatePicker!
+    @IBOutlet var tuesdayOpenTimePicker: NSDatePicker!
+    @IBOutlet var wednesdayOpenTimePicker: NSDatePicker!
+    @IBOutlet var thursdayOpenTimePicker: NSDatePicker!
+    @IBOutlet var fridayOpenTimePicker: NSDatePicker!
+    @IBOutlet var saturdayOpenTimePicker: NSDatePicker!
+    @IBOutlet var sundayOpenTimePicker: NSDatePicker!
+    
+    @IBOutlet var mondayCloseTimePicker: NSDatePicker!
+    @IBOutlet var tuesdayCloseTimePicker: NSDatePicker!
+    @IBOutlet var wednesdayCloseTimePicker: NSDatePicker!
+    @IBOutlet var thursdayCloseTimePicker: NSDatePicker!
+    @IBOutlet var fridayCloseTimePicker: NSDatePicker!
+    @IBOutlet var saturdayCloseTimePicker: NSDatePicker!
+    @IBOutlet var sundayCloseTimePicker: NSDatePicker!
+    
+    /// ---for NAMING:
+    /// monday
+    /// tuesday
+    /// wednesday
+    /// thursday
+    /// friday
+    /// saturday
+    /// sunday
+    /// var shopToEdit: Shop!
+    
+    
     
     //    MARK: - Properties
     
@@ -29,4 +67,30 @@ class ViewController: NSViewController {
     
     //    MARK: - Actions
     
+    @IBAction func mondayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func tuesdayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func wednesdayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func thursdayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func fridayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func saturdayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func sundayBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func okBtnClicked(_ sender: NSButton) {
+    }
+    
+    @IBAction func cancelBtnClicked(_ sender: NSButton) {
+    }
 }
